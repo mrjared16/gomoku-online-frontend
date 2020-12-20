6f0873f6e67c7b3cfdc9e15305e1b9ec5fe32d1c
Phuc Hoang <hvphuc081299@gmail.com>
2020-12-20 18:18:10 +0700

    fix board state conflict

--- src/features/Home/pages/RoomPage.js
+++ src/features/Home/pages/RoomPage.js
@@ -1,51 +1,32 @@
-import { Button, makeStyles } from "@material-ui/core";
-import BackTo from "features/Home/components/BackTo";
-import Board from "features/Home/components/Board";
-import UserInfoInRoom from "features/Home/components/UserInfoInRoom";
-import { range } from "lodash";
-import React, { useEffect, useState } from "react";
-import { useHistory, useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { Button, makeStyles } from '@material-ui/core';
+import BackTo from 'features/Home/components/BackTo';
+import Board from 'features/Home/components/Board';
+import UserInfoInRoom from 'features/Home/components/UserInfoInRoom';
+import { range } from 'lodash';
+import React, { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { roomSocket } from 'socket/roomSocket';
 
 const DEFAULT_SIZE = 20;
-const initialBoard = range(
-  0,
-  DEFAULT_SIZE * DEFAULT_SIZE,
-  1
-).map((index) => -1);
+const initialBoard = range(0, DEFAULT_SIZE * DEFAULT_SIZE, 1).map(
+  (index) => -1
+);
 
 const useStyles = makeStyles({
   root: {
-    padding: "20px 25px",
+    padding: '20px 25px',
   },
   container: {
-    display: "flex",
+    display: 'flex',
   },
   userInfoContainer: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-around",
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
     marginLeft: 25,
   },
 });
-const defaultResponse = {
-  sizeBoard: 20,
-  board: [],
-  host: {
-    id: '1',
-    name: '',
-    username: '',
-    photo: ''
-  },
-  opponent: {
-    id: '2',
-    name: '',
-    username: '',
-    photo: ''
-  },
-  idPlayerTurn: "1",
-};
 
 function RoomPage() {
   const [sizeBoard, setSizeBoard] = useState(DEFAULT_SIZE);
@@ -62,112 +43,140 @@ function RoomPage() {
 
   useEffect(() => {
     console.log('join room', { roomID: id });
-    roomSocket.emit('join', {
-      action: 'join',
-      token: token,
-      roomID: id
-    }, (response) => {
-      setRoomState(response);
-    });
+    roomSocket.emit(
+      'join',
+      {
+        action: 'join',
+        token: token,
+        roomID: id,
+      },
+      (response) => {
+        setRoomState(response);
+      }
+    );
 
     roomSocket.on('roomEventMsg', (response) => {
       const { data, event } = response;
       console.log('receive roomEventMsg emit: ', { response });
       const handleRoomChangeEvent = {
-        'newPlayerJoined': (setState, data) => {
+        newPlayerJoined: (setState, data) => {
           setState(data);
-        }
+        },
       };
       handleRoomChangeEvent[event](setRoomState, data);
     });
 
     return () => {
-      roomSocket.off('roomEventMsg', () => { });
+      roomSocket.off('roomEventMsg', () => {});
     };
   }, [token]);
+  // useEffect(() => {
+  // 	fetchGameState();
+  // }, []);
+  // const fetchGameState = () => {
+  // 	const [gameState] = useState([]);
+  // 	const [currentStateIndex] = useState(0);
+  // 	const currentGameState = useMemo(
+  // 		() => gameState.slice(0, currentStateIndex),
+  // 		[currentStateIndex, gameState]
+  // 	);
 
+  // 	gameState ={
+  // 		board: [],
+  // 		turn: {
+  // 			id: '',
+  // 			remainingTime: 0
+  // 		}
+  // 	}
+  // roomState = {
+  //   host: { id: '2', name: '', username: '', photo: '' },
+  //   opponent: {},
+  //   roomStatus: 'start' | 'waiting',
+  // };
+  // }
   useEffect(() => {
     roomSocket.emit('game', {}, (response) => {
-      if (!response)
-        return;
+      if (!response) return;
       const { board } = response;
       setBoard(board);
+      // TODO: set isStart
     });
 
     roomSocket.on('gameEventMsg', (response) => {
       const { data, event } = response;
       console.log('receive gameEventMsg emit: ', { response });
-      // const getSetter = {
-      //   'onHit': hit,
-      //   'changeTurn': setIdPlayerTurn
-      // }
-      // const handleRoomChangeEvent = {
-      //   'onHit': (hit, data) => {
-      //     const { index, value } = data;
-      //     hit(index, value);
-      //   },
-      //   'changeTurn': (setTurn, data) => {
-      //     const { currentTurnPlayerID } = data;
-      //     setTurn(currentTurnPlayerID);
-      //   }
-      // };
-      if (event == 'onHit') {
-        const { index, value } = data;
-        hit(index, value);
-      }
-      if (event == 'changeTurn') {
-        const { currentTurnPlayerID } = data;
-        setIdPlayerTurn(currentTurnPlayerID);
-      }
-      // handleRoomChangeEvent[event](getSetter[event], data);
+      const getSetter = {
+        onHit: hit,
+        onStart: setIsStart,
+        changeTurn: setIdPlayerTurn,
+      };
+      const handleRoomChangeEvent = {
+        onStart: (setIsStart, data) => {
+          setIsStart(true);
+        },
+        onHit: (hit, data) => {
+          const { index, value } = data;
+          hit(index, value);
+        },
+        changeTurn: (setTurn, data) => {
+          if (!isStart) {
+            setIsStart(true);
+          }
+          const { currentTurnPlayerID } = data;
+          setTurn(() => {
+            return currentTurnPlayerID;
+          });
+        },
+      };
+      handleRoomChangeEvent[event](getSetter[event], data);
     });
     return () => {
-      roomSocket.off('gameEventMsg', () => { });
+      roomSocket.off('gameEventMsg', () => {});
     };
-  }, [isStart]);
+  }, []);
 
   const handleStartGame = () => {
     setIsStart(true);
     setIdPlayerTurn(host.id);
     roomSocket.emit('start', {
-      roomID: id
+      roomID: id,
     });
-  }
+  };
 
   const setRoomState = (response) => {
-    console.log({ response });
-    const { host, opponent, currentTurnPlayerID, boardSize, board } = response;
+    const { host, opponent, boardSize } = response;
     setHost(host);
     setOpponent(opponent);
     setSizeBoard(boardSize);
-  }
+  };
 
-  const isTurn = (player, currentTurnPlayerId) => isStart && player && player.id == currentTurnPlayerId
+  const isTurn = (player, currentTurnPlayerId) =>
+    isStart && player && player.id === currentTurnPlayerId;
 
   const handleBackTo = () => {
-    history.push("/");
+    history.push('/');
   };
 
   const hit = (index, value) => {
-    board[index] = value;
-    setBoard([...board]);
-  }
+    setBoard((currentBoard) => {
+      const newBoard = [...currentBoard];
+      newBoard[index] = value;
+      return newBoard;
+    });
+  };
 
   const handleSquareClick = (index) => {
-    if (!isStart)
-      return;
-    if (board[index] !== -1)
-      return;
-    if (!isTurn(currentUserInfo, idPlayerTurn))
-      return;
-    const value = (currentUserInfo.id == host.id) ? 0 : 1;
+    if (!isStart) return;
+    if (board[index] !== -1) return;
+    if (!isTurn(currentUserInfo, idPlayerTurn)) return;
+    const value = currentUserInfo.id === host.id ? 0 : 1;
     hit(index, value);
 
     roomSocket.emit('hit', {
       roomID: id,
       index: index,
-      value: value
-    })
+      value: value,
+    });
     // if (isTurn(host, idPlayerTurn)) {
     //   board[index] = 0;
     //   setIdPlayerTurn(opponent.id);
@@ -203,7 +212,7 @@ function RoomPage() {
             variant="contained"
             color="primary"
             className="caro-button"
-            disabled={(!host || host.id != currentUserInfo.id) || (isStart)}
+            disabled={!host || host.id != currentUserInfo.id || isStart}
             onClick={handleStartGame}
           >
             Start
