7d7e9723ecae093c2372ac2147fe85e343cb2671
mrjared16 <doannguyentanphuc99@gmail.com>
2020-12-23 13:01:22 +0700

    fix hit data from index to position

--- src/features/Home/pages/RoomPage.js
+++ src/features/Home/pages/RoomPage.js
@@ -11,9 +11,6 @@ import { gameSocket } from 'socket/gameSocket';
 import axiosClient from "api/axiosClient";
 
 const DEFAULT_SIZE = 20;
-const initialBoard = range(0, DEFAULT_SIZE * DEFAULT_SIZE, 1).map(
-  (index) => -1
-);
 
 const handleRoomChangeEvent = {
   newPlayerJoined: (setState, data) => {
@@ -26,8 +23,8 @@ const handleRoomChangeEvent = {
 
 const handleGameEvent = {
   onHit: (hit, data) => {
-    const { index, value } = data;
-    hit(index, value);
+    const { position, value } = data;
+    hit(position, value);
   },
   changeTurn: (setTurn, data) => {
     const { currentTurnPlayerID } = data;
@@ -54,12 +51,17 @@ const useStyles = makeStyles({
 });
 
 function RoomPage() {
-  const [sizeBoard, setSizeBoard] = useState(DEFAULT_SIZE);
-  const [board, setBoard] = useState(initialBoard);
   const [XPlayer, setXPlayer] = useState(null);
   const [OPlayer, setOPlayer] = useState(null);
-  const [idPlayerTurn, setIdPlayerTurn] = useState(null);
+
+  const [sizeBoard, setSizeBoard] = useState(DEFAULT_SIZE);
   const [gameID, setGameID] = useState(null);
+
+  const [idPlayerTurn, setIdPlayerTurn] = useState(null);
+
+  const [gameMoves, setGameMoves] = useState([]);
+  // const [board, setBoard] = useState(initialBoard);
+
   const { token, currentUserInfo } = useSelector((state) => state.user);
   const classes = useStyles();
 
@@ -67,7 +69,14 @@ function RoomPage() {
   const { id: roomID } = useParams();
 
   const isStart = useMemo(() => (gameID !== null), [gameID]);
-
+  const board = useMemo(() => {
+    const size = sizeBoard * sizeBoard;
+    const newBoard = Array(size).fill(-1);
+    gameMoves.forEach(({ position, value }) => {
+      newBoard[position] = value;
+    });
+    return newBoard;
+  }, [gameMoves, sizeBoard])
 
   const handleBackTo = () => {
     history.push('/');
@@ -108,7 +117,7 @@ function RoomPage() {
   // 		() => gameState.slice(0, currentStateIndex),
   // 		[currentStateIndex, gameState]
   // 	);
-  
+
   const setRoomState = (response) => {
     // roomState = {
     //   host: { id: '2', name: '', username: '', photo: '' },
@@ -205,25 +214,21 @@ function RoomPage() {
   const isTurn = (player, currentTurnPlayerId) =>
     isStart && player && player.id === currentTurnPlayerId;
 
-  const hit = (index, value) => {
-    setBoard((currentBoard) => {
-      const newBoard = [...currentBoard];
-      newBoard[index] = value;
-      return newBoard;
-    });
+  const hit = (position, value) => {
+    setGameMoves(currentMoves => currentMoves.concat([{ position, value }]));
   };
 
-  const handleSquareClick = (index) => {
+  const handleSquareClick = (position) => {
     if (!isStart) return;
-    if (board[index] !== -1) return;
+    if (board[position] !== -1) return;
     if (!isTurn(currentUserInfo, idPlayerTurn)) return;
     const value = currentUserInfo.id === XPlayer.id ? 0 : 1;
-    hit(index, value);
+    hit(position, value);
 
     gameSocket.emit('hit', {
       roomID: roomID,
       gameID: gameID,
-      index: index,
+      position: position,
       value: value,
     });
   };
