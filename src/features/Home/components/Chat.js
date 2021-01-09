a82e5af4f474e06ecf830adcbba8c4a5950be363
Phuc Hoang <hvphuc081299@gmail.com>
2021-01-09 18:14:48 +0700

    hide input chat when watching history

--- src/features/Home/components/Chat.js
+++ src/features/Home/components/Chat.js
@@ -1,5 +1,12 @@
 import {
-	Box, FormControl, Icon, IconButton, InputAdornment, makeStyles, OutlinedInput, Typography
+	Box,
+	FormControl,
+	Icon,
+	IconButton,
+	InputAdornment,
+	makeStyles,
+	OutlinedInput,
+	Typography,
 } from '@material-ui/core';
 import { Form, Formik } from 'formik';
 import React from 'react';
@@ -48,7 +55,7 @@ const useStyles = makeStyles({
 		marginLeft: 5,
 		fontSize: '0.875rem',
 	},
-})
+});
 
 const initialValues = {
 	text: '',
@@ -58,67 +65,83 @@ const validationMessageSchema = Yup.object().shape({
 	text: Yup.string().trim().required(),
 });
 
-function Chat({ list = [], onSubmit = () => {} }) {
+function Chat({ list = [], onSubmit = () => { } }) {
 	const classes = useStyles();
-	const { isWatchingHistory } = useSelector(state => state.history);
+	const { isWatchingHistory } = useSelector((state) => state.history);
 
 	const renderMessage = (message) => {
 		const { username, text, createdAt } = message;
 		return (
-			<Box display='flex' marginBottom={1}>
+			<Box display="flex" marginBottom={1}>
 				<span className={classes.username}>{`${username}`}</span>
-				<span className={classes.time}>{`[${moment(createdAt).format('HH:mm:ss')}]:`}</span>
+				<span className={classes.time}>{`[${moment(createdAt).format(
+					'HH:mm:ss'
+				)}]:`}</span>
 				<span className={classes.text}>{`${text}`}</span>
 			</Box>
-		)
-	}
+		);
+	};
 
 	return (
 		<div className={classes.root}>
-			<Box display="flex" justifyContent="center" className={classes.containerTitle} >
-				<Typography variant="subtitle1" className={classes.title}>Chat</Typography>
+			<Box
+				display="flex"
+				justifyContent="center"
+				className={classes.containerTitle}
+			>
+				<Typography variant="subtitle1" className={classes.title}>
+					Chat
+        </Typography>
 			</Box>
 			<div className={classes.body}>
-				{list.map(message => renderMessage(message))}
+				{list.map((message) => renderMessage(message))}
 			</div>
-			<div className={classes.footer} id="input-message">
-				<Formik initialValues={initialValues} validationSchema={validationMessageSchema} onSubmit={onSubmit}>
-					{({ values, handleSubmit, handleChange, handleBlur }) => (
-						<Form>
-							<FormControl variant="outlined" size="small" fullWidth>
-								<OutlinedInput
-									value={values.text}
-									onChange={handleChange}
-									name="text"
-									placeholder="Aaa..."
-									autoComplete="off"
-									disabled={isWatchingHistory}
-									multiline
-									onKeyDown={(event) => {
-										if (event.key === 'Enter' && !event.shiftKey) {
-											const inputMessage = document.getElementById('input-message');
-											inputMessage.getElementsByTagName("textarea")[0].style.height = "auto";
-											handleSubmit(values);
-											event.preventDefault();
+			{!isWatchingHistory && (
+				<div className={classes.footer} id="input-message">
+					<Formik
+						initialValues={initialValues}
+						validationSchema={validationMessageSchema}
+						onSubmit={onSubmit}
+					>
+						{({ values, handleSubmit, handleChange, handleBlur }) => (
+							<Form>
+								<FormControl variant="outlined" size="small" fullWidth>
+									<OutlinedInput
+										value={values.text}
+										onChange={handleChange}
+										name="text"
+										placeholder="Aaa..."
+										autoComplete="off"
+										multiline
+										onKeyDown={(event) => {
+											if (event.key === 'Enter' && !event.shiftKey) {
+												const inputMessage = document.getElementById(
+													'input-message'
+												);
+												inputMessage.getElementsByTagName(
+													'textarea'
+												)[0].style.height = 'auto';
+												handleSubmit(values);
+												event.preventDefault();
+											}
+										}}
+										endAdornment={
+											<InputAdornment position="end">
+												<IconButton onClick={handleSubmit} edge="end">
+													<Icon
+														className="fas fa-paper-plane"
+														style={{ color: '#939b62' }}
+													/>
+												</IconButton>
+											</InputAdornment>
 										}
-									}}
-									endAdornment={
-										<InputAdornment position="end">
-											<IconButton
-												onClick={handleSubmit}
-												edge="end"
-												disabled={isWatchingHistory}
-											>
-												<Icon className="fas fa-paper-plane" style={{color: '#939b62'}} />
-                			</IconButton>
-										</InputAdornment>
-									}
-								/>
-							</FormControl>
-						</Form>
-					)}
-				</Formik>
-			</div>
+									/>
+								</FormControl>
+							</Form>
+						)}
+					</Formik>
+				</div>
+			)}
 		</div>
 	);
 }
