4311670c8b0b8dcc094f0f927309c253562996f5
Phuc Hoang <hvphuc081299@gmail.com>
2021-02-27 11:47:59 +0700

    fix loading ui v2

--- src/components/PrivateRoomRoute.js
+++ src/components/PrivateRoomRoute.js
@@ -8,56 +8,73 @@ import { showToast } from 'utils/showToast';
 import Loading from './Loading';
 
 const PrivateRoomRoute = ({ component: Component, ...rest }) => {
-	const [verify, setVerify] = useState(false);
-	const [loadingVerify, setLoadingVerify] = useState(true);
-	const [openModalPassword, setOpenModalPassword] = useState(false);
-	const history = useHistory();
-	const [passwordRoom, setPasswordRoom] = useState(null);
-	const [triggerSubmit, setTriggerSubmit] = useState(0);
-	const dispatch = useDispatch();
-	
-	const location = useLocation();
-	const { pathname } = location;
-	const roomID = pathname.split('/').pop();
-	
-	useEffect(() => {
-		setLoadingVerify(true);
-		roomApi.verifyRoom(roomID, passwordRoom).then((response) => {
-			setVerify(true);
-			setLoadingVerify(false);
-		}).catch((err) => {
-			const { statusCode, message } = err.response?.data;
-			if (statusCode === 403) {
-				setOpenModalPassword(true);
-				if (passwordRoom) {
-					showToast('error', message);
-				}
-			} else if (statusCode === 404) {
-				showToast('error', message);
-				dispatch(removeRoomID());
-				history.push('/');
-			}
-			setVerify(false);
-			setLoadingVerify(false);
-		})
-	}, [triggerSubmit])
+  const [verify, setVerify] = useState(false);
+  const [loadingVerify, setLoadingVerify] = useState(true);
+  const [openModalPassword, setOpenModalPassword] = useState(false);
+  const history = useHistory();
+  const [passwordRoom, setPasswordRoom] = useState(null);
+  const [triggerSubmit, setTriggerSubmit] = useState(0);
+  const dispatch = useDispatch();
 
-	const handleSubmitPassword = (password) => {
-		setTriggerSubmit((prev) => prev + 1);
-		setPasswordRoom(() => password);
-	}
+  const location = useLocation();
+  const { pathname } = location;
+  const roomID = pathname.split('/').pop();
+
+  useEffect(() => {
+    setLoadingVerify(true);
+    roomApi
+      .verifyRoom(roomID, passwordRoom)
+      .then((response) => {
+        setVerify(true);
+        setLoadingVerify(false);
+      })
+      .catch((err) => {
+        const { statusCode, message } = err.response?.data;
+        if (statusCode === 403) {
+          setOpenModalPassword(true);
+          if (passwordRoom) {
+            showToast('error', message);
+          }
+        } else if (statusCode === 404) {
+          showToast('error', message);
+          dispatch(removeRoomID());
+          history.push('/');
+        }
+        setVerify(false);
+        setLoadingVerify(false);
+      });
+  }, [triggerSubmit]);
+
+  const handleSubmitPassword = (password) => {
+    setTriggerSubmit((prev) => prev + 1);
+    setPasswordRoom(() => password);
+  };
 
   return (
     <>
       {loadingVerify ? (
-        <Loading />
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'var(--color-background)',
+          }}
+        >
+          <Loading />
+        </div>
       ) : (
         <Route
           {...rest}
-					render={(props) => verify && <Component {...props} />}
+          render={(props) => verify && <Component {...props} />}
         />
       )}
-			{!loadingVerify && !verify && <ModalInputPassword open={true} toggle={() => setOpenModalPassword(!openModalPassword)} onSubmit={handleSubmitPassword} />}
+      {!loadingVerify && !verify && (
+        <ModalInputPassword
+          open={true}
+          toggle={() => setOpenModalPassword(!openModalPassword)}
+          onSubmit={handleSubmitPassword}
+        />
+      )}
     </>
   );
 };
